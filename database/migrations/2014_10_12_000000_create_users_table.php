2428d9f8190241cb8937b0c1fd994de0b3eda943
Edvaldo Torres <edvaldodev@gmail.com>
2022-03-25 01:54:35 -0300

    feat: migrate de users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,33 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+
+            // access default
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            // default
+            $table->string('nickname')->nullable();
+            $table->string('document', 11)->unique()->comment('cpf')->nullable();
+            $table->date('birth_date')->nullable();
+            $table->string('genre')->comment('Man, Woman, Both, None, Preferred not to inform')->nullable();
+            $table->string('zipcode', 8)->nullable();
+
+            // teacher
+            $table->boolean('teacher_admin')->comment('true, false')->nullable();
+            $table->string('graduation')->comment('Graduating, Graduate, Specialist, Mastering, Teacher, PhD student, Doctor, Post-Doctoral, Post-Doctor')->nullable();
+            $table->string('telephone', 11)->nullable();
+            $table->boolean('whatsApp')->comment('true, false')->nullable();
+
+            // student
+            $table->string('color_declaration')->comment('Afrodescendant, Indigenous, Yellow, Black1, White, Black2, Brown')->nullable();
+            $table->text('observation')->nullable();
+
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
