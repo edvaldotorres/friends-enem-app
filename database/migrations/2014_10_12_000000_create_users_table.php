10c44e0ceb7d2fd7b8fe9684df650bb361a9944c
Edvaldo Torres <edvaldodev@gmail.com>
2022-04-01 04:29:10 -0300

    refactor: Refatoração de migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -26,18 +26,18 @@ return new class extends Migration
             $table->string('nickname')->nullable();
             $table->string('document', 11)->unique()->comment('cpf')->nullable();
             $table->date('birth_date')->nullable();
-            $table->string('genre')->comment('Man, Woman, Both, None, Preferred not to inform')->nullable();
+            $table->integer('genre')->comment('1 = Man, 2 = Woman, 3 = Both, 4 = None, 5 = Preferred not to inform')->nullable();
             $table->string('zipcode', 8)->nullable();
 
             // teacher
             $table->boolean('teacher_admin')->default(false)->comment('true, false')->nullable();
             $table->boolean('teacher')->default(false)->comment('true, false')->nullable();
-            $table->string('graduation')->comment('Graduating, Graduate, Specialist, Mastering, Teacher, PhD student, Doctor, Post-Doctoral, Post-Doctor')->nullable();
+            $table->integer('graduation')->comment('1 = Graduating, 2 = Graduate, 3 = Specialist, 4 = Mastering, 5 = Teacher, 6 = PhD student, 7 = Doctor, 8 = Post-Doctoral, 9 = Post-Doctor')->nullable();
             $table->string('telephone', 11)->nullable();
             $table->boolean('whatsapp')->default(false)->comment('true, false')->nullable();
 
             // student
-            $table->string('color_declaration')->comment('Afrodescendant, Indigenous, Yellow, Black1, White, Black2, Brown')->nullable();
+            $table->integer('color_declaration')->comment('1 = Afrodescendant, 2 = Indigenous, 3 = Yellow, 4 = Black1, 5 = White, 6 = Black2, 7 = Brown')->nullable();
             $table->text('observation')->nullable();
 
             $table->rememberToken();
