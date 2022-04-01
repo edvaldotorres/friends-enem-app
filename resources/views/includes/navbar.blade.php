1ee2ef8144919da1dd6a625348c92a1b34be13f2
Edvaldo Torres <edvaldodev@gmail.com>
2022-04-01 00:04:16 -0300

    feat: Add Logout button

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -50,12 +50,14 @@
                     Activity Log
                 </a>
                 <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Logout
+                <a class="dropdown-item" href="{{ route('logout') }}" data-toggle="modal" data-target="#logoutModal"
+                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>{{ __('Logout') }}
                 </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
             </div>
         </li>
-
     </ul>
 </nav>
