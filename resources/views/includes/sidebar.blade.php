7aba19a208538df481522cdd6de1002f2c764a8f
Edvaldo Torres <edvaldodev@gmail.com>
2022-04-04 05:29:33 -0300

    refactor:  Blade

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -112,10 +112,8 @@
         <button class="rounded-circle border-0" id="sidebarToggle"></button>
     </div>
     <div class="sidebar-card d-none d-lg-flex">
-        <img class="sidebar-card-illustration mb-2" src="img/undraw_rocket.svg" alt="...">
-        <p class="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components,
-            and more!</p>
-        <a class="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to
-            Pro!</a>
+        <p class="text-center mb-2"><strong>ENEM FELIZ -</strong> Quaisquer dúvidas, favor entrar em contato</p>
+        <a class="btn btn-success btn-sm" href="https://api.whatsapp.com/send?phone=5581997252471"
+            target="_blank">Suporte</a>
     </div>
 </ul>
