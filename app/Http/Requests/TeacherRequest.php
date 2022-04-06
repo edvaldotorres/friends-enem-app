d90d97f47ec2840bffd7b6b74379a680229433a7
Edvaldo Torres <edvaldodev@gmail.com>
2022-04-06 09:57:12 -0300

    refactor: Migrate e request

--- app/Http/Requests/TeacherRequest.php
+++ app/Http/Requests/TeacherRequest.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Requests;
 
+// use Illuminate\Validation\Rule;
+
 class TeacherRequest extends BaseRequest
 {
     public function all($keys = null)
@@ -23,12 +25,30 @@ class TeacherRequest extends BaseRequest
      */
     public function rules()
     {
+        switch ($this->route()->getName()) {
+            case 'teachers.edit':
+            case 'teachers.update':
+
+                // $email = ['required', Rule::unique('users')->ignore(2)];
+                // $email = Rule::unique('users')->ignore(2);
+
+                $document = 'required|min:11|max:14';
+                $email = 'required|email';
+                $password = 'nullable|string|min:8|confirmed';
+                break;
+            default:
+                $document = 'required|min:11|max:14|unique:users,document';
+                $email = 'required|email|unique:users,email';
+                $password = 'required|string|min:8|confirmed';
+                break;
+        }
+        
         return [
             'teacher_admin' => 'required|in:0,1',
             'teacher' => 'required|in:1',
             'name' => 'required|min:3|max:255',
             'nickname' => 'required',
-            'document' => (!empty($this->request->all()['id']) ? 'required|min:11|max:14|unique:users,document,' . $this->request->all()['id'] : 'required|min:11|max:14|unique:users,document'),
+            'document' => $document,
             'genre' => 'required|in:1,2,3,4,5',
             'birth_date' => 'required|min:10|date_format:d/m/Y',
             'zipcode' => 'min:9',
@@ -36,8 +56,8 @@ class TeacherRequest extends BaseRequest
             'whatsapp' => 'required|in:0,1',
             'graduation' => 'required|in:1,2,3,4,5,6,7,8,9',
             'discipline_id' => 'required',
-            'email' => (!empty($this->request->all()['id']) ? 'required|email|unique:users,email,' . $this->request->all()['id'] : 'required|email|unique:users,email'),
-            'password' => (empty($this->request->all()['id']) ? 'required|string|min:8|confirmed' : ''),
+            'email' => $email,
+            'password' => $password,
         ];
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,6 +23,7 @@ return new class extends Migration
             $table->string('password');
 
             // default
+            $table->boolean('teacher')->default(false)->comment('true, false')->nullable();
             $table->string('nickname')->nullable();
             $table->string('document', 11)->unique()->comment('cpf')->nullable();
             $table->date('birth_date')->nullable();
@@ -31,7 +32,6 @@ return new class extends Migration
 
             // teacher
             $table->boolean('teacher_admin')->default(false)->comment('true, false')->nullable();
-            $table->boolean('teacher')->default(false)->comment('true, false')->nullable();
             $table->integer('graduation')->comment('1 = Graduating, 2 = Graduate, 3 = Specialist, 4 = Mastering, 5 = Teacher, 6 = PhD student, 7 = Doctor, 8 = Post-Doctoral, 9 = Post-Doctor')->nullable();
             $table->string('telephone', 11)->nullable();
             $table->boolean('whatsapp')->default(false)->comment('true, false')->nullable();
