262672044c15d83c91170d349c1f6d022e52e4bc
Edvaldo Torres <edvaldodev@gmail.com>
2022-04-15 18:29:16 -0300

    fix: correção blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,8 +9,10 @@
 @section('content')
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+        @can('admin')
+            <a href="{{ route('classrooms.create') }}" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
+                <i class="fas fa-solid fa-plus"></i> Cadastar</a>
+        @endcan
     </div>
     <div class="row">
         <div class="col-xl-3 col-md-6 mb-4">
