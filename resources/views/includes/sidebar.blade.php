9249b85db0667aba66d9a6bc4ebc5b739dbb86c7
Edvaldo Torres <edvaldodev@gmail.com>
2022-04-15 20:42:48 -0300

    fix; correção blade navbar

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -13,45 +13,53 @@
     </li>
     <hr class="sidebar-divider">
     <div class="sidebar-heading">
-        Gestão de aulas
+        Gestão de diciplinas
     </div>
     <li class="nav-item">
         <a class="nav-link" href="{{ route('disciplines.index') }}">
+            <i class="fas fa-solid fa-plus"></i>
+            <span>Cadastrar</span></a>
+        <a class="nav-link" href="{{ route('disciplines.index') }}">
             <i class="fas fa-fw fa-table"></i>
             <span>Diciplinas</span></a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
-            aria-controls="collapseTwo">
-            <i class="fas fa-fw fa-tablet-alt"></i>
-            <span>Aulas</span>
-        </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <a class="collapse-item" href="{{ route('classrooms.index') }}">Listagem</a>
-                <a class="collapse-item" href="{{ route('classrooms.create') }}">Cadastro</a>
-            </div>
-        </div>
     </li>
     <hr class="sidebar-divider">
     <div class="sidebar-heading">
         Gestão de usuários
     </div>
     <li class="nav-item">
-        <a class="nav-link" href="{{ route('teachers.index') }}">
-            <i class="fas fa-solid fa-user-shield"></i>
-            <span>Professores</span></a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="{{ route('students.index') }}">
+        <a class="nav-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
+            aria-controls="collapseTwo">
             <i class="fas fa-solid fa-users"></i>
-            <span>Alunos</span></a>
+            <span>Usuários</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item" href="{{ route('teachers.index') }}">Professores</a>
+                <a class="collapse-item" href="{{ route('students.index') }}">Alunos</a>
+            </div>
+        </div>
+    </li>
+    <hr class="sidebar-divider d-none d-md-block">
+    <div class="sidebar-heading">
+        Gestão de perfil
+    </div>
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('logout') }}"
+            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+            <span>Sair</span>
+        </a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+            @csrf
+        </form>
     </li>
     <hr class="sidebar-divider d-none d-md-block">
     <div class="text-center d-none d-md-inline">
         <button class="rounded-circle border-0" id="sidebarToggle"></button>
     </div>
     <div class="sidebar-card d-none d-lg-flex">
+        <img class="sidebar-card-illustration mb-2" src="img/undraw_rocket.svg" alt="...">
         <p class="text-center mb-2"><strong>ENEM FELIZ -</strong> Quaisquer dúvidas, favor entrar em contato</p>
         <a class="btn btn-success btn-sm" href="https://api.whatsapp.com/send?phone=5581997252471"
             target="_blank">Suporte</a>
