7140ca68058e74db0f730c3baeb4cfa896694d38
Edvaldo Torres <edvaldodev@gmail.com>
2022-04-15 23:14:30 -0300

    feat: can blade laravel

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -11,35 +11,37 @@
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span></a>
     </li>
-    <hr class="sidebar-divider">
-    <div class="sidebar-heading">
-        Gestão de diciplinas
-    </div>
-    <li class="nav-item">
-        <a class="nav-link" href="{{ route('disciplines.index') }}">
-            <i class="fas fa-solid fa-plus"></i>
-            <span>Cadastrar</span></a>
-        <a class="nav-link" href="{{ route('disciplines.index') }}">
-            <i class="fas fa-fw fa-table"></i>
-            <span>Diciplinas</span></a>
-    </li>
-    <hr class="sidebar-divider">
-    <div class="sidebar-heading">
-        Gestão de usuários
-    </div>
-    <li class="nav-item">
-        <a class="nav-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
-            aria-controls="collapseTwo">
-            <i class="fas fa-solid fa-users"></i>
-            <span>Usuários</span>
-        </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <a class="collapse-item" href="{{ route('teachers.index') }}">Professores</a>
-                <a class="collapse-item" href="{{ route('students.index') }}">Alunos</a>
-            </div>
+    @can('admin')
+        <hr class="sidebar-divider">
+        <div class="sidebar-heading">
+            Gestão de diciplinas
         </div>
-    </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('disciplines.index') }}">
+                <i class="fas fa-solid fa-plus"></i>
+                <span>Cadastrar</span></a>
+            <a class="nav-link" href="{{ route('disciplines.index') }}">
+                <i class="fas fa-fw fa-table"></i>
+                <span>Diciplinas</span></a>
+        </li>
+        <hr class="sidebar-divider">
+        <div class="sidebar-heading">
+            Gestão de usuários
+        </div>
+        <li class="nav-item">
+            <a class="nav-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
+                aria-controls="collapseTwo">
+                <i class="fas fa-solid fa-users"></i>
+                <span>Usuários</span>
+            </a>
+            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <a class="collapse-item" href="{{ route('teachers.index') }}">Professores</a>
+                    <a class="collapse-item" href="{{ route('students.index') }}">Alunos</a>
+                </div>
+            </div>
+        </li>
+    @endcan
     <hr class="sidebar-divider d-none d-md-block">
     <div class="sidebar-heading">
         Gestão de perfil
